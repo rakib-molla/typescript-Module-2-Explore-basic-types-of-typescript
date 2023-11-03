748b9a5eadab0d045ad3a9a0b07163b2e2b46320
rakib-molla <rakibmail1997@gmail.com>
2023-11-03 23:55:59 +0600

    asseration

--- module2/2.1.ts
+++ module2/2.1.ts
@@ -30,9 +30,26 @@ anything = "Next level web development";
   console.log((error as CustomError).message);
  }
 
+const convertData = (value: string | number): string | number | undefined =>{
+ if( typeof value === 'string'){
+  const data = parseFloat(value)*100
+ }
+ if(typeof value === 'number'){
+  return value*1000
+ }
+}
 
+const rak = convertData(1000) as number
+const rak1 = convertData("1000") as string
 
+type Er ={
+ message: string;
+}
 
+try{
 
+}catch(error){
+ console.log((error as Er).message) 
+}
 
 }
